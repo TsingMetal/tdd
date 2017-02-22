25ccaf4adf7950dfc50ae926e23fe3a5fe843f86
TsingMetal <420895449@qq.com>
2017-02-22 08:47:36 +0800

    Enforce model validation in list view

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,4 +1,5 @@
 from unittest import skip
+from selenium.webdriver.common.keys import Keys
 from .base import FunctionalTest
 import time
 
@@ -9,7 +10,8 @@ class ItemValidationTest(FunctionalTest):
         # Edith goes to the homepage and accidentally tries to submit 
         # an empty list item. She hits Enter on the empty input box
         self.browser.get(self.server_url)
-        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+        self.browser.find_element_by_id('id_new_item') \
+                .send_keys(Keys.ENTER)
         time.sleep(1) # must wait for the browser to update
 
         # the homepage refreshes and there is an error message saying
@@ -19,12 +21,13 @@ class ItemValidationTest(FunctionalTest):
 
         # She tries again with some tex for the item, which now works
         self.browser.find_element_by_id('id_new_item')\
-                .send_keys('Buy milk\n')        
+                .send_keys('Buy milk' + Keys.ENTER)        
         time.sleep(1) # must wait for the browser to update
         self.check_for_row_in_list_table('1: Buy milk')
 
         # Perversely, she now decide to submit a second blank list item
-        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+        self.browser.find_element_by_id('id_new_item') \
+                .send_keys(Keys.ENTER)
         time.sleep(1) # must wait for the browser to update
 
         # She receives a similar warning on the list page
@@ -34,6 +37,7 @@ class ItemValidationTest(FunctionalTest):
 
         # And she can correct it by filling some text in
         self.browser.find_element_by_id('id_new_item')\
-                .send_keys('Make tea\n')
+                .send_keys('Make tea' + Keys.ENTER)
+        time.sleep(1) # must wait for the browser to update
         self.check_for_row_in_list_table('1: Buy milk')
         self.check_for_row_in_list_table('2: Make tea')
